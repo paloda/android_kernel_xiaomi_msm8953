62591c271c294f57fc4e234fb97d2bdb7f541ff7
msdx321 <msdx321@gmail.com>
2017-03-23 12:58:22 +0800

    misc: swtich to linaro 5.4.1 toolchains

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ BUILDING_DIR=$OUT_DIR/kernel_obj
 JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 DATE=`date +%m-%d-%H:%M`
 
-CROSS_COMPILER=/home/msdx321/workspace/android/toolchains/linaro-4.9.4/bin/aarch64-linux-gnu-
+CROSS_COMPILER=/home/msdx321/workspace/android/toolchains/linaro-5.4.1/bin/aarch64-linux-gnu-
 
 ANYKERNEL_DIR=$ROOT_DIR/misc/anykernel2
 TEMP_DIR=$OUT_DIR/temp
@@ -50,7 +50,7 @@ FUNC_PACK()
 		cp $BUILDING_DIR/arch/arm64/boot/Image.gz-dtb $TEMP_DIR/zImage-dtb
         mkdir $TEMP_DIR/modules
         find . -type f -name "*.ko" | xargs cp -t $TEMP_DIR/modules
-        find $TEMP_DIR -iname "*.ko" -exec /home/msdx321/workspace/android/toolchains/linaro-4.9.4/bin/aarch64-linux-gnu-strip --strip-debug {} \;
+        find $TEMP_DIR -iname "*.ko" -exec /home/msdx321/workspace/android/toolchains/linaro-5.4.1/bin/aarch64-linux-gnu-strip --strip-debug {} \;
 		cd $TEMP_DIR
 		zip -r9 mKernel.zip ./*
 		mv mKernel.zip $OUT_DIR/mKernel-$DATE.zip
