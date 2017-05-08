16a85b13014473ae8731cd7170eddd0140879c4e
paloda <paloda09@gmail.com>
2017-05-08 20:35:25 +0200

    init build
    # Conflicts:
    #	build_kernel.sh

---
+++ build_kernel.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+export ARCH=arm64
+
+ROOT_DIR=$(pwd)
+OUT_DIR=$ROOT_DIR/out
+BUILDING_DIR=$OUT_DIR/kernel_obj
+
+JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
+DATE=`date +%m-%d-%H:%M`
+
+CROSS_COMPILER=/home/paloda/android/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+ANYKERNEL_DIR=$ROOT_DIR/misc/anykernel2
+TEMP_DIR=$OUT_DIR/temp
+
+DEFCONFIG=markw_defconfig
+
+FUNC_PRINT()
+{
+		echo ""
+		echo "=============================================="
+		echo $1
+		echo "=============================================="
+		echo ""
+}
+
+FUNC_CLEAN()
+{
+		FUNC_PRINT "Cleaning All"
+		rm -rf $OUT_DIR
+		mkdir $OUT_DIR
+		mkdir -p $BUILDING_DIR
+		mkdir -p $TEMP_DIR
+}
+
+FUNC_COMPILE_KERNEL()
+{
+		FUNC_PRINT "Start Compiling Kernel"
+		make -C $ROOT_DIR O=$BUILDING_DIR $DEFCONFIG 
+		make -C $ROOT_DIR O=$BUILDING_DIR -j$JOB_NUMBER ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILER
+		FUNC_PRINT "Finish Compiling Kernel"
+}
+
+
+FUNC_PACK()
+{
+		FUNC_PRINT "Start Packing"
+		cp -r $ANYKERNEL_DIR/* $TEMP_DIR
+		cp $BUILDING_DIR/arch/arm64/boot/Image.gz-dtb $TEMP_DIR/zImage-dtb
+        mkdir $TEMP_DIR/modules
+        find . -type f -name "wlan.ko" | xargs cp -t $TEMP_DIR/modules
+        find $TEMP_DIR -iname "wlan.ko" -exec /home/paloda/android/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-strip --strip-debug {} \;
+		cd $TEMP_DIR
+		zip -r9 PalodaKernel.zip ./*
+		mv PalodaKernel.zip $OUT_DIR/PalodaKernel-$DATE.zip
+		cd $ROOT_DIR
+		FUNC_PRINT "Finish Packing"
+}
+
+START_TIME=`date +%s`
+FUNC_CLEAN
+FUNC_COMPILE_KERNEL
+FUNC_PACK
+END_TIME=`date +%s`
+
+let "ELAPSED_TIME=$END_TIME-$START_TIME"
+echo "Total compile time is $ELAPSED_TIME seconds"
