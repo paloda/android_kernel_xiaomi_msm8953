62d87844fd1707463f695869f738c819664c688d
paloda <paloda09@gmail.com>
2017-05-28 19:29:18 +0200

    updated toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ BUILDING_DIR=$OUT_DIR/kernel_obj
 JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 DATE=`date +%m-%d-%H:%M`
 
-CROSS_COMPILER=/home/paloda/android/toolchains/aarch64-5.3-linaro/bin/aarch64-linux-android-
+CROSS_COMPILER=/home/paloda/android/aarch64-5.3/bin/aarch64-linux-android-
 
 ANYKERNEL_DIR=$ROOT_DIR/misc/anykernel2
 TEMP_DIR=$OUT_DIR/temp
@@ -50,10 +50,10 @@ FUNC_PACK()
 		cp $BUILDING_DIR/arch/arm64/boot/Image.gz-dtb $TEMP_DIR/zImage-dtb
         mkdir $TEMP_DIR/modules
         find . -type f -name "wlan.ko" | xargs cp -t $TEMP_DIR/modules
-        find $TEMP_DIR -iname "wlan.ko" -exec /home/paloda/android/toolchains/aarch64-5.3-linaro/bin/aarch64-linux-strip --strip-debug {} \;
+        find $TEMP_DIR -iname "wlan.ko" -exec /home/paloda/android/aarch64-5.3/bin/aarch64-linux-strip --strip-debug {} \;
 		cd $TEMP_DIR
 		zip -r9 PalodaKernel.zip ./*
-		mv PalodaKernel.zip $OUT_DIR/PalodaKernel-$DATE.zip
+		mv PalodaKernel.zip $OUT_DIR/PalodaKernelR3-$DATE.zip
 		cd $ROOT_DIR
 		FUNC_PRINT "Finish Packing"
 }
